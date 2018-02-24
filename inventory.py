adc841d629ca35e4f9b81edbb949bf125c47e3de
舟木正憲 <funakimfunaki@gmail.com>
2018-02-24 16:56:53 +0900

    5章練習問題実装

---
+++ inventory.py
@@ -0,0 +1,11 @@
+stuff = {'ロープ': 1, 'たいまつ': 6, '金貨': 42, '手裏剣': 1, '矢': 12}
+
+def display_inventory(inventory):
+    print("持ち物リスト:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        item_total += v
+    print("アイテム総数: " + str(item_total))
+
+display_inventory(stuff)
